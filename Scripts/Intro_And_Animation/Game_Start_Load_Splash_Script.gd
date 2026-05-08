e712e79f3388d25c7ef95cdf6f735a6040a834f7
APINKSHRIMP <Olly95@live.co.uk>
2026-05-08 15:26:44 +0100

    Finished Layout/aestethics of Verdant Forest

--- Scripts/Intro_And_Animation/Game_Start_Load_Splash_Script.gd
+++ Scripts/Intro_And_Animation/Game_Start_Load_Splash_Script.gd
@@ -7,9 +7,9 @@ extends Node2D
 
 var audio_player := AudioStreamPlayer.new()
 
-const GROW_DURATION := 5 # 7.0
-const BLACK_FADE_DURATION := 5 # 7
-const SCENE_FADE_DURATION := 2 # 3
+const GROW_DURATION := 0.2 # 7.0
+const BLACK_FADE_DURATION := 0.2 # 7
+const SCENE_FADE_DURATION := 0.2 # 3
 
 # Where to send the player after the splash. The actual destination is the
 # scene they were last in (resumed from save). If no save exists, falls
